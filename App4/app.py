4dd020c16b3bf84f9629756b25cd93ab58f5c166
Gokulnath S <131845355+Gokulsgn@users.noreply.github.com>
2024-08-30 12:08:19 +0530

    U

--- App4/app.py
+++ App4/app.py
@@ -10,10 +10,12 @@ import pickle
 
 def load_model():
     model_path = os.path.join(os.path.dirname(__file__), 'logistic_regression_model.pkl')
+    print(f"Loading model from: {model_path}")
     if not os.path.isfile(model_path):
         raise FileNotFoundError(f"Model file not found: {model_path}")
     with open(model_path, 'rb') as file:
         model = pickle.load(file)
+        print("Model successfully loaded.")
     return model
 
 model = load_model()
